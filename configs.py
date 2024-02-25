3f66aeb6842b2400dab6fb24d5a746aa0d7bbb8c
Ibrahim khan <108752662+luciferworld@users.noreply.github.com>
2024-02-25 15:10:33 +0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,21 +3,21 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
-	SHORTLINK_API = os.environ.get('SHORTLINK_API')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	API_ID = int(os.environ.get("API_ID", "25184668"))
+	API_HASH = os.environ.get("API_HASH", "9de2fcd18b25deed06adf855fcd181ed")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6992601655:AAGkcnUNIY8K7nNTthxbTZHhRp7HOgvHIQ8")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "filestoreee_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001805072460"))
+	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
+	SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5390385209"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Ibrahim:ibrahim@cluster0.zuppk7u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001934362597")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", '-1002027405917')
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS",).split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
@@ -25,28 +25,30 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ **🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅**]────⍟
 │
-├🔸 **My Name:** [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 **My Name:** [File Store Bot](https://t.me/{BOT_USERNAME})
 │
 ├🔸 **Language:** [Python 3](https://www.python.org)
 │
 ├🔹 **Library:** [Pyrogram](https://docs.pyrogram.org)
 │
-├🔹 **Hosted On:** [Heroku](https://heroku.com)
+├🔹 **Hosted On:** [Vps](https://cloud.atlantic.net/r/cggqrw65)
 │
-├🔸 **Developer:** [Predator HackerzZ](https://t.me/OwnYourBotz) 
+├🔸 **Owner:** [Ibrahim](https://t.me/lucifer6985) 
 │
-├🔹 **Bot Support:** [Support Group](https://t.me/TeleRoid14)
+├🔹 **File Stream bot :** [FileStream](https://t.me/Liciferfiletolink_bot)
 │
-├🔸 **Bot Updates:** [Bots Channel](https://t.me/TeleRoidGroup)
+├🔹 **Free Ai Logo Maker :** [LogoMaker](https://t.me/ChatGPT2112_bot?start=5390385209)
+│
+├🔸 **Bot Updates:** [Bots Channel](https://t.me/digitalhub04)
 │
 ╰──────[ 😎 ]───────────⍟
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@PredatorHackerzZ](https://github.com/PredatorHackerzZ)
+🧑🏻‍💻 **Bot owner:** [Ibrahim](https://t.me/lucifer6985)
  
- I am Super noob Please Support My Hard Work.
+ I am Super noob Please and i am not developer of this bot.
 
-[Donate Me](https://t.me/DonateXrobot) or ```MrAbhi2k3@apl```
+[Developer](https://t.me/DonateXrobot)
 """
 	HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
